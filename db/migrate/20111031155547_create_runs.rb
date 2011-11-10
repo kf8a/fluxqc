d436f1c8a8d2d8e7ae4927f6009f46ef5fed4cbe
Sven Bohm <bohms@msu.edu>
2011-11-10 12:46:15 -0500

    backfilled attributes on run

--- db/migrate/20111031155547_create_runs.rb
+++ db/migrate/20111031155547_create_runs.rb
@@ -1,6 +1,10 @@
 class CreateRuns < ActiveRecord::Migration
   def change
     create_table :runs do |t|
+      t.date      :sampled_on
+      t.date      :run_on
+      t.string    :study
+      t.text      :comment
       t.string    :name
       t.string    :workflow_state
 
