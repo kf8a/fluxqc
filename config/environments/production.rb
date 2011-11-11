c42661c7f2c7876cbf43ae0e728eab1b307bb0f7
Sven Bohm <bohms@msu.edu>
2011-11-10 20:51:08 -0500

    added host parameter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Fluxqc::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  config.action_mailer.default_url_options = { :host => 'gprpc28.kbs.msu.edu:5000' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
