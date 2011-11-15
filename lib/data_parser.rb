78845a9f0d6a56e7e948a19c1e3ef21636dcecd1
Sven Bohm <bohms@msu.edu>
2011-11-15 12:21:48 -0500

    single line parsing

--- lib/data_parser.rb
+++ lib/data_parser.rb
@@ -1,6 +1,12 @@
+require 'csv'
 class DataParser
 
   attr_accessor :data
+
+  def parse_line(data)
+    parsed_data = CSV.parse_line(data, :col_sep=>" ")
+    {:vial=>parsed_data[7].to_i, :ch4=>{:value=>parsed_data[10].to_f}, :co2=>{:value => parsed_data[14].to_f}, :n2o=>{:value => parsed_data[18].to_f}}
+  end
 
   def parse
     true
--- spec/models/data_parser_spec.rb
+++ spec/models/data_parser_spec.rb
@@ -2,6 +2,11 @@ require File.expand_path("../../../lib/data_parser.rb",__FILE__)
 
 describe DataParser do
   describe 'parsing a line of data' do
+    it 'is succesful' do
+      parser = DataParser.new
+      data = "19 09/26/11 11:24:15 AM  18  26-Sep-11, 11:18:46 4 CH4 0.363163  38.807774 38.807774 co2 1.991239  71590.445312  71590.445312  N2O 4.327775  388.435394  388.435394  AutoInt Z:\GLBR    C SER11 092611 TRY3\GLBRC SER11 092611 TRY3 2011-09-26 09-19-16\4.D"
+      parser.parse_line(data).should == {:vial=>4, :ch4=>{:value => 38.807774}, :co2=>{:value => 71590.445312}, :n2o=>{:value=>388.435394} } 
+    end
 
   end
 
