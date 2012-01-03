56c1e8cfa9c72b7eed3407d2030b86195acf96cd
Sven Bohm <bohms@msu.edu>
2012-01-03 08:56:22 -0500

    made the row assignmenet a little clearer

--- lib/setup_parser.rb
+++ lib/setup_parser.rb
@@ -19,33 +19,34 @@ class SetupParser
 
   def parse_csv(file)
     lines = CSV::readlines(file)
-    5.times { lines.shift }
-    result = []
-    lines.each do |row|
-     next if row[0].nil?
-
-     result << parse_sample(row)
+    5.times { lines.shift } # remove the header limes TODO this is different for GLBRC
+    lines.collect do |row|
+     parse_sample(row)
     end
-    result
   end
 
   def parse_sample(row)
+    treatment = row[0].to_s
+    replicate = row[1].to_s
+    chamber   = row[3].to_s
+    vial      = row[4].to_s
+    lid       = row[5].to_s
+    height    = [row[6].to_f,row[7].to_f, row[8].to_f, row[9].to_f]
+    soil_temp = row[10].to_f
+
     seconds = row[15]
     if seconds.class.name == 'Spreadsheet::Formula'
       seconds = seconds.value
     end
+    seconds = seconds.to_f
 
     comments = row[16]
     comments = nil if comments == '-'
 
-    {:treatment => row[0].to_s,
-      :replicate => row[1].to_s,
-      :chamber => row[3].to_s,
-      :vial => row[4].to_s, 
-      :lid => row[5], 
-      :height => [row[6].to_f,row[7].to_f, row[8].to_f, row[9].to_f],
-      :soil_temperature => row[10].to_f,
-      :seconds => seconds.to_f,
-      :comments => comments }
+    {:treatment => treatment, :replicate => replicate,
+      :chamber => chamber, :vial => vial,
+      :lid => lid, :height => height,
+      :soil_temperature => soil_temp, 
+      :seconds => seconds, :comments => comments }
   end
 end
