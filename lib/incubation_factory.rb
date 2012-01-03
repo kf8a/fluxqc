8d61b4b273ef41e229260c8a71eaa4e4556e8fb8
Sven Bohm <bohms@msu.edu>
2012-01-03 16:47:23 -0500

    reuse incubations

--- lib/incubation_factory.rb
+++ lib/incubation_factory.rb
@@ -1,16 +1,21 @@
 class IncubationFactory
 
   def self.create(sample = {})
-    incubation = Incubation.find_by_treatment_replicate_chamber_sampled_at(sample[:treatment], sample[:replicate], sample[:chamber], sample[:sample_at])
+    incubation = Incubation.where(:treatment => sample[:treatment],
+                                 :replicate  => sample[:replicate],
+                                 :chamber    => sample[:chamber],
+                                 :sampled_at=> sample[:sampled_at]).first
 
-    incubation = incubation || Incubation.new
-    incubation.treatment          = sample[:treatment]
-    incubation.replicate          = sample[:replicate]
-    incubation.chamber            = sample[:chamber]
-    incubation.soil_temperature   = sample[:soil_temperature]
-    incubation.average_height_cm  = sample[:height].inject(:+)/sample[:height].count
-    incubation.lid                = Lid.find_by_name(sample[:lid])
-    incubation.sampled_at         = sample[:sample_at]
+    unless incubation
+      incubation = Incubation.new
+      incubation.treatment          = sample[:treatment]
+      incubation.replicate          = sample[:replicate]
+      incubation.chamber            = sample[:chamber]
+      incubation.soil_temperature   = sample[:soil_temperature]
+      incubation.average_height_cm  = sample[:height].inject(:+)/sample[:height].count
+      incubation.lid                = Lid.find_by_name(sample[:lid])
+      incubation.sampled_at         = sample[:sample_at]
+    end
     incubation
   end
 end
