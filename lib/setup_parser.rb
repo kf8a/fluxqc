b72a1c1f7d5c40923f31711ee470d0f4496f74af
Sven Bohm <bohms@msu.edu>
2012-01-03 08:42:07 -0500

    getting the same parse hash out of both csv and xls files

--- lib/setup_parser.rb
+++ lib/setup_parser.rb
@@ -1,8 +1,9 @@
 require 'spreadsheet'
+require 'csv'
 
 class SetupParser
 
-  # This should parse the file and create the appropriate models
+  # This should parse the file and return a hash of sample objects
   def parse_xls(file)
     book = Spreadsheet.open(file)
     sheet = book.worksheets.first
@@ -11,22 +12,40 @@ class SetupParser
     sheet.each 5 do |row|
       next if row[0].nil?
      
-      seconds = row[15]
-      if seconds.class.name == 'Spreadsheet::Formula'
-        seconds = seconds.value
-      end
-        
-      sample = {:treatment => row[0], 
-                :replicate => row[1], 
-                :chamber => row[3],
-                :vial => row[4], 
-                :lid => row[5], 
-                :height => (row[6] + row[7] + row[8] + row[9])/4,
-                :soil_temperature => row[10],
-                :seconds => seconds
-               }
-      result << sample
+      result << parse_sample(row)
     end
     result
+  end
+
+  def parse_csv(file)
+    lines = CSV::readlines(file)
+    5.times { lines.shift }
+    result = []
+    lines.each do |row|
+     next if row[0].nil?
+
+     result << parse_sample(row)
+    end
+    result
+  end
+
+  def parse_sample(row)
+    seconds = row[15]
+    if seconds.class.name == 'Spreadsheet::Formula'
+      seconds = seconds.value
+    end
+
+    comments = row[16]
+    comments = nil if comments == '-'
+
+    {:treatment => row[0].to_s,
+      :replicate => row[1].to_s,
+      :chamber => row[3].to_s,
+      :vial => row[4].to_s, 
+      :lid => row[5], 
+      :height => [row[6].to_f,row[7].to_f, row[8].to_f, row[9].to_f],
+      :soil_temperature => row[10].to_f,
+      :seconds => seconds.to_f,
+      :comments => comments }
   end
 end
--- spec/models/setup_parser_spec.rb
+++ spec/models/setup_parser_spec.rb
@@ -1,20 +1,46 @@
 require File.expand_path("../../../lib/setup_parser.rb",__FILE__)
 
+RESULT1 = {:treatment => '6', :replicate=>'1', 
+           :chamber=>'1', :vial =>'1',
+           :lid=>'C', :height =>[18, 19.5, 19, 20.5], 
+           :soil_temperature => 18.5,
+           :seconds => 0.0, :comments => nil}
+RESULT2 = {:treatment => '2', :replicate=>'1',
+           :chamber=>'1', :vial => '6',
+           :lid => 'D', :height=> [19.5, 19, 19, 19], 
+           :soil_temperature => 19,
+           :seconds => 20.0, :comments => nil}
+
 describe SetupParser do
   describe 'parsing a csv setup file' do
     before do
-      @parser = SetupParser.new
+      parser = SetupParser.new
+      file = File.expand_path("../../data/setup_test.csv", __FILE__)
+      File.exists?(file).should be_true
+      @result = parser.parse_csv(file)
     end
-    it 'should be successful' do
-      pending "I'm going to implement the xls first"
-      @parser.parse("mysetupfile.csv")
+
+    it 'returns an array of sample hashes' do
+      @result.class.should == Array
+    end
+
+    it 'returns the right data for the first row' do
+      @result[0].should == RESULT1
+    end
+
+    it 'returns the right data for a string treatment' do
+      @result[1][:treatment].should == 'DF'
+    end
+
+    it 'returns the right data for another row' do
+      @result[5].should == RESULT2
     end
   end
 
   describe 'parsing a xls setup file' do
     before do
       parser = SetupParser.new
-      file = File.expand_path("../../data/test.xls", __FILE__)
+      file = File.expand_path("../../data/setup_test.xls", __FILE__)
       File.exists?(file).should be_true
       @result = parser.parse_xls(file)
     end
@@ -24,16 +50,14 @@ describe SetupParser do
     end
 
     it 'returns the right data for the first row' do
-      @result[0].should == {:treatment => 6.0, :replicate=>1.0, 
-                            :chamber=>1.0, :vial =>1.0,
-                            :lid=>'C', :height => 19.25, :soil_temperature => 18.5,
-                            :seconds => 0.0}
+      @result[0].should == RESULT1
+    end
+
+    it 'returns the right data for a string treatment' do
+      @result[1][:treatment].should == 'DF'
     end
     it 'returns the right data for another row' do
-      @result[5].should == {:treatment => 2.0, :replicate=>1.0,
-                            :chamber=>1.0, :vial => 6.0,
-                            :lid => 'D', :height=> 19.125, :soil_temperature => 19.0,
-                            :seconds => 20.0}
+      @result[5].should == RESULT2
     end
   end
 
