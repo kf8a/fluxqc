5fef4200e262750903ab806b297f52b21461046a
Sven Bohm <bohms@msu.edu>
2012-04-11 12:58:18 -0400

    detect if we are processing a chemstation file

--- lib/data_parser.rb
+++ lib/data_parser.rb
@@ -3,9 +3,13 @@ require 'csv'
 class DataParser
 
   def self.parse(file_name)
-    lines = CSV::readlines(file_name) # , :encoding => "UTF-16LE:UTF-8", :col_sep=>"\t")
-    #look for first row that starts with a number
+    lines = CSV::readlines(file_name, :encoding => "UTF-16LE:UTF-8", :col_sep=>"\t")
 
+    # see if we have a chemstation file
+    if lines[0][0] =~ /Analysis Date/
+      chemstation = true
+    end
+    #look for first row that starts with a number
     first_row_of_data = 0
     lines.each do |row|
       if row[1] =~ /\d+/
@@ -22,9 +26,14 @@ class DataParser
     # attached to the bottom of the results file. This results in multiple copies of the data in the file.
     # We want to use the last version of the data for the vial that is in the file
     vials = {}
-    lines.each do |row|
-    #  vials[row[1]] = {:vial=>row[3], :ch4=>row[7].to_f, :co2=>row[11].to_f, :n2o=>row[15].to_f}
-      vials[row[0]] = {:vial=>row[0], :n2o=>row[1].to_f, :co2=>row[2].to_f, :ch4=>row[3].to_f}
+    if chemstation
+      lines.each do |row|
+        vials[row[1]] = {:vial=>row[3], :ch4=>row[7].to_f, :co2=>row[11].to_f, :n2o=>row[15].to_f}
+      end
+    else
+      lines.each do |row|
+        vials[row[0]] = {:vial=>row[0], :n2o=>row[1].to_f, :co2=>row[2].to_f, :ch4=>row[3].to_f}
+      end
     end
     vials.values
   end
