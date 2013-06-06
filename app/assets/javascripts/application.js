8aafa67628266f554adf3aebcad72eb9d518f789
Sven Bohm <bohms@msu.edu>
2013-06-05 22:01:59 -0400

    moved files around

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require d3
 //= require underscore
 //= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require d3
-//= require backbone/fluxqc
+//= require fluxqc
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ app/assets/javascripts/fluxqc.js.coffee
@@ -0,0 +1,5 @@
+window.Flux =
+  Models: {}
+  Collections: {}
+  Routers: {}
+  Views: {}
