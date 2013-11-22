a17e3dca02f5f9f278bfc28c4036ac877c994120
Sven Bohm <bohms@msu.edu>
2013-11-22 15:40:25 -0500

    show subplot info

--- app/serializers/incubation_serializer.rb
+++ app/serializers/incubation_serializer.rb
@@ -1,5 +1,12 @@
 class IncubationSerializer < ActiveModel::Serializer
-  attributes :id, :avg_height_cm, :chamber, :treatment, :replicate, :sampled_at, 
-    :soil_temperature, :run_id, :co2, :ch4, :n2o
+  attributes :id, :avg_height_cm, :sampled_at, 
+    :soil_temperature, :run_id, :co2, :ch4, :n2o, :name
+
+  def name
+    result = "#{object.treatment}#{object.replicate}"
+    result += "-#{object.chamber}"  if object.chamber
+    result += "-#{object.sub_plot}" if object.sub_plot
+    result
+  end
 
 end
