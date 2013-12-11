3e04ede06a9b0828cadea4eba10ff5e4d872762b
Sven Bohm <bohms@msu.edu>
2013-12-11 13:04:36 -0500

    revert the column addition it should be on the sample

--- db/migrate/20131211143108_add_location_to_measuremnts_and_standards.rb
+++
@@ -1,6 +0,0 @@
-class AddLocationToMeasuremntsAndStandards < ActiveRecord::Migration
-  def change
-    add_column :measurements, :column, :integer
-    add_column :standards, :column, :integer
-  end
-end
