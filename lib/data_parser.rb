f21529e25a2b73dd03cce61b8f3dd06992b62dd2
Sven Bohm <bohms@msu.edu>
2013-12-13 13:34:42 -0500

    use the area column not the amount

--- lib/data_parser.rb
+++ lib/data_parser.rb
@@ -48,9 +48,9 @@ class DataParser
 
   def chemstation_parse(row)
     {:vial=>row[3],
-      :ch4=>{:area => row[7].to_f},
-      :co2=>{:area => row[11].to_f}, 
-      :n2o=>{:column => row[1].to_i % 2, :area => row[15].to_f}}
+      :ch4=>{:area => row[6].to_f},
+      :co2=>{:area => row[10].to_f}, 
+      :n2o=>{:column => row[1].to_i % 2, :area => row[14].to_f}}
   end
 
   def processed_parse(row)
