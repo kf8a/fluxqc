4071feec32b4f481854b140f8fe9ef76feb7adbf
Sven Bohm <bohms@msu.edu>
2013-12-18 11:33:45 -0500

    remove secret for version control

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Fluxqc::Application.config.secret_key_base = 'a3b2d61df5108e3f25f3746c4b183fb06ddd9db4d3d055b21bc4e9b8cd5179f9cfb17a17cd8a6fe5670f9c65f49a1b8afef9562a5b7d986aa0d98c70bce9da18'
+Fluxqc::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
