471d73d9097ec23c11a7bed6aa662fb212912833
Sven Bohm <bohms@msu.edu>
2014-04-07 16:14:31 -0400

    use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Fluxqc::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.default_url_options = { :host => 'fluxqc.kbs.msu.edu' }
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
