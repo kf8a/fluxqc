3b7dafa6936e850b712c2a24935599dfacbb8e9a
Sven Bohm <bohms@msu.edu>
2014-04-15 20:14:06 -0400

    changed spec to take into account the workflow state of the run

--- spec/controllers/fluxes_controller_spec.rb
+++ spec/controllers/fluxes_controller_spec.rb
@@ -10,13 +10,16 @@ describe FluxesController do
 
   describe 'PUT update point' do
     before do
+      @run          = FactoryGirl.create :run
       @flux         = FactoryGirl.create :flux
       @measurement  = FactoryGirl.create :measurement, flux_id: @flux.id
+
     end
 
     it 'is successful' do
       @flux.stub(:save).and_return(true)
-      p @user.company
+      Measurement.any_instance.stub(:standard_curves).and_return(1)
+      Flux.any_instance.stub(:run).and_return(@run)
       Flux.any_instance.stub(:company).and_return("lter")
       Flux.any_instance.stub(:headspace).and_return(1.2)
       Flux.any_instance.stub(:surface_area).and_return(4.5)
