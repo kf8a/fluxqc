9e686899a8bc42fd154c977655eb360e30072865
Sven Bohm <bohms@msu.edu>
2014-04-23 16:32:59 -0400

    authenticate for everything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 # Root application controller
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!, except: [:index]
+  before_filter :authenticate_user! #, except: [:index]
 end
