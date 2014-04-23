f2ef5d1f8bcca7c9be1fab868e48764ddc2d0f3d
Sven Bohm <bohms@msu.edu>
2014-04-23 14:51:15 -0400

    adds tests for some more cases

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -9,42 +9,54 @@ describe Measurement do
     measurement.respond_to?(:mv).should be_true
   end
 
-  it 'is selectable by compound' do
-    compound    = FactoryGirl.create :compound, :name=>'co2'
-    measurement = FactoryGirl.create :measurement, :compound => compound
-    flux        = FactoryGirl.create :flux, :measurements => [measurement]
+  it "is selectable by compound" do
+    compound    = FactoryGirl.create :compound, name: "co2"
+    measurement = FactoryGirl.create :measurement, compound: compound
+    flux        = FactoryGirl.create :flux, measurements: [measurement]
     Measurement.by_compound('co2').include?(measurement).should be_true
   end
 
-  describe 'locating standard curves' do
+  describe "locating standard curves" do
     before(:each) do
-      @compound    = FactoryGirl.create :compound, :name=>'co2'
+      @compound    = FactoryGirl.create :compound, name: "co2"
       @run         = FactoryGirl.create :run
-      sample      = FactoryGirl.create :sample, run: @run
+      sample       = FactoryGirl.create :sample, run: @run
       @curve1      = FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()-2.hours, run: @run
       @curve2      = FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()+2.hours, run: @run
       @measurement = FactoryGirl.create :measurement, compound: @compound, acquired_at: Time.now(), sample: sample
     end
 
-    it 'ignores later standard curves' do
-      curve3      = FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()+4.hours, run: @run
+    it "ignores later standard curves" do
+      FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()+4.hours, run: @run
       @measurement.standard_curves().should == [@curve1, @curve2]
     end
 
-    it 'ignores earlier standard curves' do
-      curve3      = FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()-4.hours, run: @run
+    it "ignores earlier standard curves" do
+      FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()-4.hours, run: @run
       @measurement.standard_curves().should == [@curve1, @curve2]
     end 
 
-    it 'ignores other runs curves' do
+    it "ignores other runs curves" do
       run = FactoryGirl.create :run
-      curve3      = FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()-4.hours, run: run
+      FactoryGirl.create :standard_curve, compound: @compound, acquired_at: Time.now()-4.hours, run: run
       @measurement.standard_curves().should == [@curve1, @curve2]
     end
 
-    it 'returns what is there' do
+    it "ignores other compounds standard curves" do
+      compound    = FactoryGirl.create :compound, name: "H"
+      FactoryGirl.create :standard_curve, compound: compound, acquired_at: Time.now()+4.hours, run: @run
+      @measurement.standard_curves().should == [@curve1, @curve2]
+    end
+
+    it "returns what is there" do
       @curve2.delete
       @measurement.standard_curves().should == [@curve1]
+    end
+
+    it "returns nothing if there is nothing" do
+      @curve1.delete
+      @curve2.delete
+      @measurement.standard_curves().should == []
     end
   end
 end
