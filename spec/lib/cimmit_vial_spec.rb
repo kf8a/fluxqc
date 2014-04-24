9c7dd1eff8c4da32d728b2bca67f58d9d4ecb6a8
Sven Bohm <bohms@msu.edu>
2014-04-24 15:53:30 -0400

    adds more special cases for cimmyt vials

--- spec/lib/cimmit_vial_spec.rb
+++ spec/lib/cimmit_vial_spec.rb
@@ -4,6 +4,12 @@ describe CimmitVial do
   it "deals with a dash and CIM name" do
     CimmitVial.process_cimmit_vial("S3-CIM-B-101-T0").should == "S3-CIM-B-101-T0"
   end
+  it "deals with a dash and CIM name and space before the B" do
+    CimmitVial.process_cimmit_vial("S3-CIM- B-101-T0").should == "S3-CIM-B-101-T0"
+  end
+  it "deals with a space after CIM" do
+    CimmitVial.process_cimmit_vial("S3-CIM B-101-T0").should == "S3-CIM-B-101-T0"
+  end
   it "deals with a dash between the series and the rest" do
     CimmitVial.process_cimmit_vial("S3-F-101-T0").should == "S3-F-101-T0"
   end
