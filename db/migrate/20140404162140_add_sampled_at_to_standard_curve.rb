e62d1b151d8243941be043d39ac2bcc0489408f9
Sven Bohm <bohms@msu.edu>
2014-04-24 09:01:30 -0400

    use acquired_at right away and delete the later renaming. i had already created it in production manually

--- db/migrate/20140404162140_add_sampled_at_to_standard_curve.rb
+++ db/migrate/20140404162140_add_sampled_at_to_standard_curve.rb
@@ -1,5 +1,5 @@
 class AddSampledAtToStandardCurve < ActiveRecord::Migration
   def change
-    add_column :standard_curves, :sampled_at, :timestamp
+    add_column :standard_curves, :acquired_at, :timestamp
   end
 end
--- db/migrate/20140423174128_rename_sampled_at_to_acquired_at.rb
+++
@@ -1,5 +0,0 @@
-class RenameSampledAtToAquiredAt < ActiveRecord::Migration
-  def change
-    rename_column :standard_curves, :sampled_at, :acquired_at
-  end
-end
