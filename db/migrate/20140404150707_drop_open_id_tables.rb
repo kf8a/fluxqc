98c760061d5964f58cf71389ad446f46bda58d16
Sven Bohm <bohms@msu.edu>
2015-01-19 12:07:31 -0500

    remove the drop open id tables since we don't create them anymore

--- db/migrate/20140404150707_drop_open_id_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropOpenIdTables < ActiveRecord::Migration
-  def change
-    drop_table :open_id_authentication_associations
-    drop_table :open_id_authentication_nonces
-    remove_column :users, :openid_identifier
-  end
-end
