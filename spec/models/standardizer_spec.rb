76b825878b557527a22d05a8e74347afa01c73a3
Sven Bohm <bohms@msu.edu>
2015-01-22 09:20:21 -0500

    moved standardizer to models and updated to rspec3 syntax

--- spec/models/standardizer_spec.rb
+++ spec/models/standardizer_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Standardizer do
   let(:st) { Standardizer.new }
@@ -6,58 +6,58 @@ describe Standardizer do
   it 'should take standard curves' do
     st = Standardizer.new
     st.standard_curves = []
-    st.standard_curves.should == []
+    expect(st.standard_curves).to eq []
   end
 
   describe 'to_ppm' do
     describe 'without standard curves' do
       it 'returns nil unless we have standard curves' do
         m = Measurement.new
-        m.stub(:area).and_return(100.0)
+        allow(m).to receive(:area).and_return(100.0)
         st.to_ppm(m)
-        m.ppm.should == nil
+        expect(m.ppm).to eq nil
       end
     end
 
     describe 'with standard curves' do
       before do
         @curve1 = StandardCurve.new
-        @curve1.stub(:slope).and_return(2)
-        @curve1.stub(:intercept).and_return(1)
+        allow(@curve1).to receive(:slope).and_return(2)
+        allow(@curve1).to receive(:intercept).and_return(1)
         st.standard_curves = [@curve1]
 
         @m = Measurement.new
-        @m.stub(:area).and_return(100.0)
+        allow(@m).to receive(:area).and_return(100.0)
       end
 
       it 'computes ppm from one standard curve' do
         st.to_ppm(@m)
-        @m.ppm.should == 201.0
+        expect(@m.ppm).to eq 201.0
       end
 
       it 'computes ppm from two standard curves as the average' do
         curve2 = StandardCurve.new
-        curve2.stub(:slope).and_return(4)
-        curve2.stub(:intercept).and_return(1)
+        allow(curve2).to receive(:slope).and_return(4)
+        allow(curve2).to receive(:intercept).and_return(1)
         st.standard_curves = [@curve1, curve2]
 
         st.to_ppm(@m)
-        @m.ppm.should == 301.0
+        expect(@m.ppm).to eq 301.0
       end
 
       it 'computes ppm from two curves with drift correction' do 
         curve2 = StandardCurve.new
-        curve2.stub(:slope).and_return(4)
-        curve2.stub(:intercept).and_return(1)
+        allow(curve2).to receive(:slope).and_return(4)
+        allow(curve2).to receive(:intercept).and_return(1)
 
-        @curve1.stub(:position).and_return(1)
-        curve2.stub(:position).and_return(10)
+        allow(@curve1).to receive(:position).and_return(1)
+        allow(curve2).to receive(:position).and_return(10)
         st.standard_curves = [@curve1, curve2]
 
-        @m.stub(:position).and_return(3)
+        allow(@m).to receive(:position).and_return(3)
 
         st.to_ppm_with_drift_correction(@m)
-        @m.ppm.should be_within(0.1).of(245.44)
+        expect(@m.ppm).to be_within(0.1).of(245.44)
       end
     end
   end
