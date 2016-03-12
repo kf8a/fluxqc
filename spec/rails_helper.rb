49653a2bb7cd6db9188edc5d57c5966064c68b96
Sven Bohm <bohms@msu.edu>
2016-03-11 21:00:41 -0500

    updated shoulda:matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,3 +49,14 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.include Devise::TestHelpers, :type => :controller
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
+
