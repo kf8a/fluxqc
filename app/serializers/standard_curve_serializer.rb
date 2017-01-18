b810386ee9804cac9acf0fbd7bb68034785775ec
Sven Bohm <bohms@msu.edu>
2017-01-18 09:55:33 -0500

    updated standard_curve serializer to return the same as the old one

--- app/serializers/standard_curve_serializer.rb
+++ app/serializers/standard_curve_serializer.rb
@@ -1,3 +1,4 @@
 class StandardCurveSerializer < ActiveModel::Serializer
-  attributes :id, :slope, :intercept
+  attributes :id, :slope, :intercept, :compound_id, :run_id,
+    :data, :fit_line, :compound
 end
