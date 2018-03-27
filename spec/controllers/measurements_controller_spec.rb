388e1fe7681c8272be08b438e9012f2c79dc792f
Sven Bohm <bohms@msu.edu>
2018-03-27 12:09:03 -0400

    style fixes

--- spec/controllers/measurements_controller_spec.rb
+++ spec/controllers/measurements_controller_spec.rb
@@ -1,16 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe MeasurementsController, type: :controller do
-
   describe "GET 'index' as an authenticated user" do
     before(:each) do
       @user = FactoryBot.create(:user)
       sign_in @user
     end
-    it "returns http success" do
+    it 'returns http success' do
       get 'index'
       expect(response).to be_success
     end
   end
-
 end
