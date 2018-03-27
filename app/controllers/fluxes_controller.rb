24797896e2908a198260db8b5aabb68e6ca33b38
Sven Bohm <bohms@msu.edu>
2018-03-27 12:15:19 -0400

    style fixes

--- app/controllers/fluxes_controller.rb
+++ app/controllers/fluxes_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class FluxesController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
 
   respond_to :html, :json
 
--- spec/controllers/fluxes_controller_spec.rb
+++ spec/controllers/fluxes_controller_spec.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe FluxesController, :type => :controller do
-
+describe FluxesController, type: :controller do
   before(:each) do
     @user = FactoryBot.create(:user)
-    allow(@user).to receive(:company).and_return("lter")
+    allow(@user).to receive(:company).and_return('lter')
     sign_in @user
   end
 
@@ -13,20 +14,19 @@ describe FluxesController, :type => :controller do
       @run          = FactoryBot.create :run
       @flux         = FactoryBot.create :flux
       @measurement  = FactoryBot.create :measurement, flux_id: @flux.id
-
     end
 
     it 'is successful' do
       allow(@flux).to receive(:save).and_return(true)
       allow_any_instance_of(Measurement).to receive(:standard_curves).and_return(1)
       allow_any_instance_of(Flux).to receive(:run).and_return(@run)
-      allow_any_instance_of(Flux).to receive(:company).and_return("lter")
+      allow_any_instance_of(Flux).to receive(:company).and_return('lter')
       allow_any_instance_of(Flux).to receive(:headspace).and_return(1.2)
       allow_any_instance_of(Flux).to receive(:surface_area).and_return(4.5)
       allow_any_instance_of(Flux).to receive(:mol_weight).and_return(12.3)
-      post :update, id: @flux.id , data: [{id: @measurement.id, key: 40, value: 1.69, area: 33.34, deleted: true}]
-      expect(response.code).to eq "200"
+      post :update, params: { id: @flux.id, data: [{ id: @measurement.id, key: 40,
+                                                     value: 1.69, area: 33.34, deleted: true }] }
+      expect(response.code).to eq '200'
     end
   end
-
 end
