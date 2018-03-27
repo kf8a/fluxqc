1c921d2e07cd2275576181e5a88aeb31647fe59a
Sven Bohm <bohms@msu.edu>
2018-03-27 12:14:59 -0400

    remove deprecated flag

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,6 @@ module Fluxqc
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.active_record.raise_in_transactional_callbacks = true
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
