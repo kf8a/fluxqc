b765ba3f9db2ec67f609583870d669fd62875693
Sven Bohm <bohms@msu.edu>
2018-03-27 11:20:33 -0400

    style fixes

--- spec/controllers/runs_controller_spec.rb
+++ spec/controllers/runs_controller_spec.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe RunsController, :type => :controller do
-
+describe RunsController, type: :controller do
   before(:each) do
     @user = FactoryGirl.create(:user)
     sign_in @user
   end
 
   describe "GET 'index'" do
-    it "returns http success" do
+    it 'returns http success' do
       get :index
       expect(response).to be_success
     end
@@ -22,7 +23,7 @@ describe RunsController, :type => :controller do
     before(:all) do
       @run = FactoryGirl.create :run
     end
-    it "returns http success" do
+    it 'returns http success' do
       get :show, id: @run
       expect(response).to be_success
     end
@@ -35,10 +36,10 @@ describe RunsController, :type => :controller do
   describe 'GET updated_at' do
     render_views
 
-    before(:all) do 
+    before(:all) do
       @run = FactoryGirl.create :run
     end
-    it "returns http success" do
+    it 'returns http success' do
       get :updated_at, id: @run
       expect(response).to be_success
     end
@@ -62,16 +63,16 @@ describe RunsController, :type => :controller do
 
   describe ':POST create' do
     describe 'with a valid object' do
-     it 'redirects to show' do
-       allow_any_instance_of(Run).to receive(:id).and_return(1)
-       post :create , run: {name: 'test'}
-       expect(response).to redirect_to(edit_run_path(1))
-     end
+      it 'redirects to show' do
+        allow_any_instance_of(Run).to receive(:id).and_return(1)
+        post :create, run: { name: 'test' }
+        expect(response).to redirect_to(edit_run_path(1))
+      end
     end
     describe 'with an invalid object' do
       it 'redirects back to new' do
         allow_any_instance_of(Run).to receive(:save).and_return(false)
-        post :create, run: {name: 'test'}
+        post :create, run: { name: 'test' }
         expect(response).to redirect_to(new_run_path)
       end
     end
@@ -90,7 +91,7 @@ describe RunsController, :type => :controller do
       it 'redirects to show' do
         run = Run.create
         allow(Run).to receive(:find).with('1').and_return(run)
-        post :update, id: 1, run: {name: 'test'}
+        post :update, id: 1, run: { name: 'test' }
         expect(response).to redirect_to(run_path(assigns(:run)))
       end
     end
@@ -99,7 +100,7 @@ describe RunsController, :type => :controller do
         run = Run.create
         allow(Run).to receive(:find).with('1').and_return(run)
         allow(run).to receive(:valid?).and_return(false)
-        post :update, id: 1, run: {name: 'test'}
+        post :update, id: 1, run: { name: 'test' }
         expect(response).to redirect_to(edit_run_path(assigns(:run)))
       end
     end
@@ -107,8 +108,8 @@ describe RunsController, :type => :controller do
 
   describe 'computing the standards' do
     it 'computes the standard equation'
-      # Run.any_instance.sub(:standardize).and_return(true)
-      # Run.any_instance.stub(:id).and_return(1)
+    # Run.any_instance.sub(:standardize).and_return(true)
+    # Run.any_instance.stub(:id).and_return(1)
     it 'computes measurement ppms using the standard'
   end
 
@@ -130,16 +131,15 @@ describe RunsController, :type => :controller do
       it 'rejects a run' do
         allow(@run).to receive('reject!').and_return(true)
         post :reject, id: @run
-        expect(response).to redirect_to(runs_path(:state=>'rejected'))
+        expect(response).to redirect_to(runs_path(state: 'rejected'))
       end
-
     end
 
     describe 'POST accept' do
       it 'accepts a run' do
         allow(@run).to receive('accept!')
         post :accept, id: @run
-        expect(response).to redirect_to(runs_path(:state=>'uploaded'))
+        expect(response).to redirect_to(runs_path(state: 'uploaded'))
       end
     end
 
@@ -147,7 +147,7 @@ describe RunsController, :type => :controller do
       it 'approves a run' do
         allow(@run).to receive('approve!')
         post :approve, id: @run
-        expect(response).to redirect_to(runs_path(:state=>'accepted'))
+        expect(response).to redirect_to(runs_path(state: 'accepted'))
       end
     end
 
@@ -155,7 +155,7 @@ describe RunsController, :type => :controller do
       it 'publishes a run' do
         allow(@run).to receive('publish!')
         post :publish, id: @run
-        expect(response).to redirect_to(runs_path(:state => 'approved'))
+        expect(response).to redirect_to(runs_path(state: 'approved'))
       end
     end
 
@@ -163,7 +163,7 @@ describe RunsController, :type => :controller do
       it 'unapproves a run' do
         allow(@run).to receive('unapprove!')
         post :unapprove, id: @run
-        expect(response).to redirect_to(runs_path(:state => 'approved'))
+        expect(response).to redirect_to(runs_path(state: 'approved'))
       end
     end
 
@@ -171,9 +171,8 @@ describe RunsController, :type => :controller do
       it 'unpublishes a run' do
         allow(@run).to receive('unpublish!').and_return(true)
         post :unpublish, id: @run
-        expect(response).to redirect_to(runs_path(:state => 'published'))
+        expect(response).to redirect_to(runs_path(state: 'published'))
       end
     end
   end
-
 end
