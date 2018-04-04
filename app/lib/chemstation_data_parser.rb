888fb0e98771f4d2a72b3a9e0f2163bb5d113927
Sven Bohm <bohms@msu.edu>
2018-04-04 09:40:01 -0400

    removed wrong comments

--- app/lib/chemstation_data_parser.rb
+++ app/lib/chemstation_data_parser.rb
@@ -7,7 +7,6 @@ class ChemstationDataParser
     lines = DataFileDecapitator.shift_header_lines(lines)
     vials = {}
     lines.each do |row|
-      p "SINGLE LOOP"
       vials[row[1]] = chemstation_parse(row)
     end
     vials.values
@@ -18,21 +17,20 @@ class ChemstationDataParser
     results[:vial] = ChemstationUtils.parse_vial(row)
     results[:acquired_at] = ChemstationUtils.parse_time(row[0])
 
-    # columns = [4, 8, 12] # the location of the compound names
-    #
-    # # if the column 3 is not a vial but looks like a date
-    # columns = [6, 10, 14] if looks_like_time?(row[3])
-    #
-    # # column 4 can be empty or col 3 is not a vial
-    # columns = [5, 9, 13] unless row[4] # if column 4 is empty then they are shifted
     columns = ChemstationUtils.locate_compound_columns(row)
     columns.each do |column|
-      # Rename 0 to o because sometimes n2o get's written as n20
-      key = row[column].downcase.tr('0', 'o').to_sym
+      key = symbolize_key(row[column])
 
       value = row[column + 2]
       results[key] = { column: row[1].to_i % 2, area: value.to_f }
     end
     results
+  end
+
+  private
+
+  def symbolize_key(name)
+    # Rename 0 to o because sometimes n2o get's written as n20
+    name.downcase.tr('0', 'o').to_sym
   end
 end
