e13dd512563079f8d91dd124b7939b0a0fb5fac9
Sven Bohm <bohms@msu.edu>
2019-01-24 11:06:07 -0500

    style fixes

--- app/lib/incubation_factory.rb
+++ app/lib/incubation_factory.rb
@@ -1,16 +1,19 @@
+# frozen_string_literal: true
+
 # This class creates incubations based on the setup file
 class IncubationFactory
+  COMPOUND_NAMES = %w[n2o co2 ch4].freeze
 
-  def self.create(run_id, input= {})
+  def self.create(run_id, input = {})
     run = Run.find(run_id)
-    incubation = run.incubations.where(:treatment => input[:treatment],
-                                 :replicate  => input[:replicate],
-                                 :sub_plot   => input[:sub_plot],
-                                 :chamber    => input[:chamber],
-                                 :sampled_at => input[:sampled_at]).first
+    incubation = run.incubations.where(treatment: input[:treatment],
+                                       replicate: input[:replicate],
+                                       sub_plot: input[:sub_plot],
+                                       chamber: input[:chamber],
+                                       sampled_at: input[:sampled_at]).first
 
     if incubation
-      ['n2o','co2','ch4'].each do |c|
+      COMPOUND_NAMES.each do |c|
         compound = Compound.find_by_name(c)
         flux = incubation.flux(c)
         update_measurement(flux, input, compound, run, incubation)
@@ -23,10 +26,10 @@ class IncubationFactory
       incubation.sub_plot           = input[:sub_plot]
       incubation.chamber            = input[:chamber]
       incubation.soil_temperature   = input[:soil_temperature]
-      incubation.avg_height_cm      = input[:height].inject(:+)/input[:height].count
+      incubation.avg_height_cm      = input[:height].inject(:+) / input[:height].count
       incubation.lid                = Lid.find_by_name(input[:lid])
       incubation.sampled_at         = input[:input_at]
-      ['n2o','co2','ch4'].each do |c|
+      COMPOUND_NAMES.each do |c|
         compound = Compound.find_by_name(c)
         flux = Flux.new
         flux.compound = compound
@@ -40,10 +43,10 @@ class IncubationFactory
     incubation
   end
 
-  def self.update_sample(measurement, run, vial,incubation)
-    sample = run.samples.where(:vial => vial).first
+  def self.update_sample(measurement, run, vial, incubation)
+    sample = run.samples.where(vial: vial).first
     unless sample
-      sample = Sample.new(:vial => vial)
+      sample = Sample.new(vial: vial)
       sample.incubation = incubation
       sample.save
       run.samples << sample
@@ -52,12 +55,12 @@ class IncubationFactory
     measurement.save
   end
 
-  def self.update_measurement(flux,input, compound, run, incubation)
+  def self.update_measurement(flux, input, compound, run, incubation)
     vial = input[:vial]
-    measurement = Measurement.new(:seconds => input[:seconds],
-                                  :compound => compound,
-                                  :vial=>vial)
+    measurement = Measurement.new(seconds: input[:seconds],
+                                  compound: compound,
+                                  vial: vial)
     flux.measurements << measurement
-    update_sample(measurement, run, vial,incubation)
+    update_sample(measurement, run, vial, incubation)
   end
 end
