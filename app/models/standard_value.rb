45aac8394cf516eca56297cc4515db4795ed6758
Sven Bohm <bohms@msu.edu>
2019-01-24 11:08:19 -0500

    style fix

--- app/models/standard_value.rb
+++ app/models/standard_value.rb
@@ -1,18 +1,4 @@
-# == Schema Information
-#
-# Table name: standard_values
-#
-#  id         :integer          not null, primary key
-#  name       :text
-#  set        :integer
-#  n2o        :float
-#  co2        :float
-#  ch4        :float
-#  valid_from :datetime
-#  valid_to   :datetime
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
+# frozen_string_literal: true
 
 class StandardValue < ActiveRecord::Base
 end
