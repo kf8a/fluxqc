62b38f365cd7fe7748b7179d13d4640f92a5a504
sven <bohms@ideomatic.io>
2020-04-15 16:24:44 -0400

    put the unicorn file where capistrano expects it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Fluxqc::Application.configure do
   config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  # config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -65,7 +65,7 @@ Fluxqc::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  
+
   config.action_mailer.default_url_options = { :host => 'fluxqc.kbs.msu.edu' }
   config.action_mailer.delivery_method = :sendmail
 
