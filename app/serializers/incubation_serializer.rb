e7a7dec35ba54eafce5b32031a12f79ee83acb86
sven <bohms@ideomatic.io>
2021-01-11 12:29:31 -0500

    style fixes

--- app/serializers/incubation_serializer.rb
+++ app/serializers/incubation_serializer.rb
@@ -1,6 +1,6 @@
 class IncubationSerializer < ActiveModel::Serializer
-  attributes :id, :avg_height_cm, :sampled_at, 
-    :soil_temperature, :run_id, :co2, :ch4, :n2o, :name
+  attributes :id, :avg_height_cm, :sampled_at,
+             :soil_temperature, :run_id, :co2, :ch4, :n2o, :name
 
   def name
     result = "#{object.treatment}#{object.replicate}"
@@ -8,5 +8,4 @@ class IncubationSerializer < ActiveModel::Serializer
     result += "-#{object.sub_plot}" if object.sub_plot
     result
   end
-
 end
