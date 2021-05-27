fead1a708a191f7564428795b7d7ccb3e97c6d2b
sven <bohms@ideomatic.io>
2021-05-27 08:32:30 -0400

    gem version bump

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -2,14 +2,3 @@ class Setup < ActiveRecord::Base
   belongs_to :template
 end
 
-# == Schema Information
-#
-# Table name: setups
-#
-#  id          :integer          not null, primary key
-#  template_id :integer
-#  sample_date :date
-#  name        :string(255)
-#  created_at  :datetime
-#  updated_at  :datetime
-#
