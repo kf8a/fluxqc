b645fa21c9728f2c950102669ce19d744377e703
Sven Bohm <bohms@hey.com>
2024-06-10 12:19:23 -0400

    update for newer ruby version

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module Fluxqc
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exist?(env_file)
     end
 
   end
