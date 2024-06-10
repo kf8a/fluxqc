49db64cc4c5db5f109614a147e6d168fd624dab4
Sven Bohm <bohms@hey.com>
2024-06-10 12:17:45 -0400

    merge local gem changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.13.0"
+lock "~> 3.18.0"
 
 set :application, "fluxqc"
 set :repo_url, "git@github.com:kf8a/fluxqc.git"
